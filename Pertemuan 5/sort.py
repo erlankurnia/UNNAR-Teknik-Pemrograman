300c3a4e734815a384c1fbfa5c3a16a9b464d2ce
Erlan K <ahlipdkno.2@gmail.com>
2020-03-10 01:11:08 +0700

    Revisi 1

--- Pertemuan 5/sort.py
+++ Pertemuan 5/sort.py
@@ -1,13 +1,18 @@
 from random import randrange
-data = []; i = 0
+# Memberikan nilai acak pada data array yang jumlahnya juga acak
 data = [randrange(0,100) for i in range(0, randrange(2,10))]
 print('Data awal\t: ', data); i = 0
+# Pengulangan pertama untuk mendapatkan elemen dengan nilai terkecil
 while i < len(data):
   j = i
+  # Pengulangan kedua untuk membandingkan nilai yang belum diurutkan
   while j < len(data):
     if (data[i] > data[j]):
-      temp = data[j]
-      data[j] = data[i]
-      data[i] = temp
+      temp = data[j]; data[j] = data[i]; data[i] = temp
     j+=1
-  print(('Iterasi ke-'+ str(i+1) + '\t: '), data); i+=1
+  i+=1
+  # Menampilkan Pengulangan yg dilakukan
+  if not i >= len(data)-1:
+    print(('Iterasi ke-'+ str(i) + '\t: '), data);
+  elif i is len(data)-1:
+    print(('Hasil Akhir\t: '), data)
